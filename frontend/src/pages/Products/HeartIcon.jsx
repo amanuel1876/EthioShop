fbf866ef7e707444798f18d3401cca5dc1448cef
amanuel1876 <amanedesaleng@gmail.com>
2024-08-26 14:53:19 +0300

    mern stack project

---
+++ frontend/src/pages/Products/HeartIcon.jsx
@@ -0,0 +1,59 @@
+import PropTypes from 'prop-types';
+import { useEffect } from "react";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  addToFavorites,
+  removeFromFavorites,
+  setFavorites,
+} from "../../redux/features/favorites/favoriteSlice";
+
+import {
+  addFavoriteToLocalStorage,
+  getFavoritesFromLocalStorage,
+  removeFavoriteFromLocalStorage,
+} from "../../Utils/localStorage";
+
+const HeartIcon = ({ product }) => {
+  const dispatch = useDispatch();
+  const favorites = useSelector((state) => state.favorites) || [];
+  const isFavorite = favorites.some((p) => p._id === product._id);
+
+  useEffect(() => {
+    const favoritesFromLocalStorage = getFavoritesFromLocalStorage();
+    dispatch(setFavorites(favoritesFromLocalStorage));
+  }, [dispatch]);
+
+  const toggleFavorites = () => {
+    if (isFavorite) {
+      dispatch(removeFromFavorites(product));
+      removeFavoriteFromLocalStorage(product._id);
+    } else {
+      dispatch(addToFavorites(product));
+      addFavoriteToLocalStorage(product);
+    }
+  };
+
+  return (
+    <div
+      className="absolute top-2 right-5 cursor-pointer"
+      onClick={toggleFavorites}
+    >
+      {isFavorite ? (
+        <FaHeart className="text-pink-500" />
+      ) : (
+        <FaRegHeart className="text-white" />
+      )}
+    </div>
+  );
+};
+
+// Adding PropTypes validation for props
+HeartIcon.propTypes = {
+  product: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    // You can add additional product properties here if necessary
+  }).isRequired,
+};
+
+export default HeartIcon;
---
+++ frontend/src/redux/features/cart/cartSlice.js
@@ -0,0 +1,61 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { updateCart } from "../../../Utils/cartUtils";
+
+const initialState = localStorage.getItem("cart")
+  ? JSON.parse(localStorage.getItem("cart"))
+  : { cartItems: [], shippingAddress: {}, paymentMethod: "PayPal" };
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      const { ...item } = action.payload;
+      const existItem = state.cartItems.find((x) => x._id === item._id);
+
+      if (existItem) {
+        state.cartItems = state.cartItems.map((x) =>
+          x._id === existItem._id ? item : x
+        );
+      } else {
+        state.cartItems.push(item); // Push the new item directly to the array
+      }
+      updateCart(state); // Update the cart in localStorage
+    },
+
+    removeFromCart: (state, action) => {
+      state.cartItems = state.cartItems.filter((x) => x._id !== action.payload);
+      updateCart(state); // Update the cart in localStorage
+    },
+
+    saveShippingAddress: (state, action) => {
+      state.shippingAddress = action.payload;
+      localStorage.setItem("cart", JSON.stringify(state));
+    },
+
+    savePaymentMethod: (state, action) => {
+      state.paymentMethod = action.payload;
+      localStorage.setItem("cart", JSON.stringify(state));
+    },
+
+    clearCartItems: (state) => {
+      state.cartItems = [];
+      localStorage.setItem("cart", JSON.stringify(state));
+    },
+
+    resetCart: () => {
+      return initialState; // Reset the state to initialState
+    },
+  },
+});
+
+export const {
+  addToCart,
+  removeFromCart,
+  savePaymentMethod,
+  saveShippingAddress,
+  clearCartItems,
+  resetCart,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
